b9d9a6f480030fc99e75facf9a25a5dcb7c5f425
anantsingh1145 <anant.singh1145@gmail.com>
2025-06-24 02:12:39 +0530

    Adding first go hello code

---
+++ beginner-exercises/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	fmt.Println(`Hey there i am using go for first time
+This is a simple Go program.
+It prints a greeting message to the console.
+Go is a statically typed, compiled programming language designed at Google.
+It is known for its simplicity and efficiency.`)
+}
