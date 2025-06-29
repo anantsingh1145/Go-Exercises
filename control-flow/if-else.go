e92469729e85e3ef98d8cb9bc2410a43ea1c75c5
anantsingh1145 <anant.singh1145@gmail.com>
2025-06-29 20:10:06 +0530

    adding if-else.go condition to understand control flow

---
+++ control-flow/if-else.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	x, y := rand.Intn(10), rand.Intn(10)
+
+	if x < 4 && y < 4 {
+		fmt.Printf("Both x = %d and y = %d are less than 4\n", x, y)
+	} else if x > 6 && y > 6 {
+		fmt.Printf("Both x = %d and y = %d are less than 6\n", x, y)
+	} else if x >= 4 && x <= 6 {
+		fmt.Printf("x = %d is between 4 and 6, and y = %d is less than or equal to 6\n", x, y)
+	} else if y != 5 {
+		fmt.Printf("y = %d is not equal to 5\n", y)
+	} else {
+		fmt.Printf("No specific condition met for x = %d and y = %d \n", x, y)
+	}
+}
