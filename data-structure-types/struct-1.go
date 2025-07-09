95ea75723b4e360d29329633c82868f328cdb057
anantsingh1145 <anant.singh1145@gmail.com>
2025-07-09 23:30:53 +0530

    Adding first struct code in go

--- data-structure-types/struct-1.go
+++ data-structure-types/struct-1.go
@@ -3,22 +3,22 @@ package main
 type person struct {
 	name string
 	age  int
-	doy  int
+	yob  int
 }
 
 func main() {
 	p1 := person{
 		name: "James",
 		age:  25,
-		doy:  2000,
+		yob:  2000,
 	}
 
 	p2 := person{
 		name: "charles",
 		age:  26,
-		doy:  1999,
+		yob:  1999,
 	}
 
-	println(p1.name, "is of age:", p1.age, "and his birth year is:", p1.doy)
-	println(p2.name, "is of age:", p2.age, "and his birth year is:", p2.doy)
+	println(p1.name, "is of age:", p1.age, "and his birth year is:", p1.yob)
+	println(p2.name, "is of age:", p2.age, "and his birth year is:", p2.yob)
 }
